f4e90d603ebed41abd0a2d0254583d2ea3b819b1
Arne Hilmann <arne@hilmann.de>
2018-01-05 15:37:59 +0100

    make filter less verbose

--- src/markdeck/lib/revealjs-shortcut-filter.lua
+++ src/markdeck/lib/revealjs-shortcut-filter.lua
@@ -3,17 +3,17 @@ local bg_attrs = {"data-background", "data-background-image"}
 function Header(elem)
     if elem.attributes then
         if elem.attributes.bg then
-            io.stderr:write("section " .. elem.identifier .. "\n")
+            -- io.stderr:write("section " .. elem.identifier .. "\n")
             local i=1
             for w in elem.attributes.bg:gmatch("([^;]*)") do
-                io.stderr:write("    " .. bg_attrs[i] .. ": " .. w .. "\n")
+                -- io.stderr:write("    " .. bg_attrs[i] .. ": " .. w .. "\n")
                 elem.attributes[bg_attrs[i]]=w
                 i=i+1
             end
             elem.attributes.bg=nil
         end
         if elem.attributes.bgcss then
-            io.stderr:write("section " .. elem.identifier .. "\n")
+            -- io.stderr:write("section " .. elem.identifier .. "\n")
             elem.attributes["data-state"]=elem.attributes.bgcss
             elem.attributes.bgcss=nil
         end
