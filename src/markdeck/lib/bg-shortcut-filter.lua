04db781ccd0f28cd33a2eb30c8ac456ee8d9dd42
Arne Hilmann <arne@hilmann.de>
2018-02-25 19:00:33 +0100

    rename variants

--- src/markdeck/lib/bg-shortcut-filter.lua
+++ src/markdeck/lib/bg-shortcut-filter.lua
@@ -3,7 +3,7 @@ local bg_attrs = {"data-background", "data-background-image"}
 function Header(elem)
     if elem.attributes then
         if elem.attributes.bg then
-            -- io.stderr:write("section " .. elem.identifier .. "\n")
+            io.stderr:write("section " .. elem.identifier .. "\n")
             local i=1
             for w in elem.attributes.bg:gmatch("([^;]*)") do
                 -- io.stderr:write("    " .. bg_attrs[i] .. ": " .. w .. "\n")
---
+++ src/markdeck/lib/impress-shortcut-filter.lua
@@ -0,0 +1,60 @@
+local change = {x=0, y=0, z=0, scale=0, rotate=0}
+local last = {x=0, y=0, z=0, scale=1, rotate=0}
+local history = {}
+
+
+function calc_pos(elem, dim)
+    local attr_name = "data-" .. dim
+    if elem.attributes[dim .. "-of"] then
+        elem.attributes[attr_name] = history[elem.attributes[dim .. "-of"]][attr_name]
+        change[dim] = 0
+    end
+    if elem.attributes[dim] then
+        elem.attributes[attr_name] = elem.attributes[dim]
+        change[dim] = 0
+    end
+    if elem.attributes[dim .. "r"] then
+        elem.attributes[attr_name] = last[dim] + elem.attributes[dim .. "r"]
+        change[dim] = elem.attributes[attr_name] - last[dim]
+    end
+    if elem.attributes[attr_name] == nil then
+        elem.attributes[attr_name] = last[dim] + change[dim]
+        change[dim] = elem.attributes[attr_name] - last[dim]
+    end
+    last[dim] = elem.attributes[attr_name]
+end
+
+
+function calc_position(elem)
+    for _, dim in pairs({"x", "y", "z", "scale", "rotate"}) do
+        calc_pos(elem, dim)
+    end
+end
+
+function remember_position(elem)
+    local entry = {}
+    for _, dim in pairs({"x", "y", "z", "scale", "rotate"}) do
+        local attr_name = "data-" .. dim
+        entry[attr_name] = elem.attributes[attr_name]
+    end
+    history[elem.identifier] = entry
+end
+
+function dump_position(elem)
+    local attrs = {elem.identifier}
+    for _, dim in pairs({"x", "y", "z", "scale", "rotate"}) do
+        local attr_name = "data-" .. dim
+        table.insert(attrs, elem.attributes[attr_name])
+    end
+    print(table.concat(attrs, " "))
+end
+
+function Header(elem)
+    table.insert(elem.classes, "step")
+
+    calc_position(elem)
+    remember_position(elem)
+    dump_position(elem)
+
+    return elem
+end
