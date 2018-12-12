2f553899918591ac57535c6df4291ea9e8c20d1f
Arne Hilmann <arne@hilmann.de>
2018-12-12 13:13:31 +0100

    start self-documentation

---
+++ docs/docs/markdown-basics/assets/markdeck/revealjs-helper.js
@@ -0,0 +1,35 @@
+function calc_offset_left() {
+    return offset_left = $(".slides").offset().left / Reveal.getScale();
+}
+
+function flushleft(selector) {
+    function deoffset() {
+        // console.log("flushleft");
+        var rel_offset_left = calc_offset_left();
+        $(selector).each(function(i, e){
+            $(this).css({"margin-left": "-" + rel_offset_left + "px"});
+        });
+    }
+    deoffset();
+    if (window.pdf_render) {
+        setTimeout(deoffset, 50);
+    } else {
+        $(window).on("resize", deoffset);
+    }
+}
+
+function centerleft(selector) {
+    function deoffset() {
+        // console.log("centerleft");
+        var rel_offset_left = calc_offset_left() / 2;
+        $(selector).each(function(i, e){
+            $(this).css({"margin-left": "-" + rel_offset_left + "px"});
+        });
+    }
+    deoffset();
+    if (window.pdf_render) {
+        setTimeout(deoffset, 50);
+    } else {
+        $(window).on("resize", deoffset);
+    }
+}
