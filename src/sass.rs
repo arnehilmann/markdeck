353bc7baa8e26ee5880c5debb0371845c0eb90c8
Arne Hilmann <arne@hilmann.de>
2021-05-14 23:53:46 +0200

    add warning

--- src/sass.rs
+++ src/sass.rs
@@ -1,11 +1,13 @@
 use std::fs::{self};
 use std::path::Path;
+use log::{info, warn};
 
 pub fn sassc(source_folder: &Path, scss_file: &str, target_folder: &Path) -> anyhow::Result<()> {
     let mut source_path = source_folder.to_path_buf();
     source_path.push(scss_file);
 
     if !source_path.exists() {
+        info!("scss not found at {}", source_path.display());
         return Ok(());
     }
 
@@ -16,9 +18,13 @@ pub fn sassc(source_folder: &Path, scss_file: &str, target_folder: &Path) -> any
     let mut target_path = target_folder.to_path_buf();
     target_path.push(scss_file);
     target_path.set_extension("css");
+    info!("compiling {} -> {}", source_path.display(), target_path.display());
     match sass_result {
         Ok(css) => fs::write(target_path, css)?,
-        Err(_) => fs::write(target_path, "")?,
+        Err(msg) => {
+            warn!("cannot compile scss: {}", msg);
+            fs::write(target_path, "")?;
+        },
     }
     Ok(())
 }
