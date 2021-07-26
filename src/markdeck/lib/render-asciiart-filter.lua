150ca4fae6fe4186da24ef98e79fa994fa736220
Arne Hilmann <arne@hilmann.de>
2021-07-26 11:10:54 +0200

    add svgbob call

--- src/markdeck/lib/render-asciiart-filter.lua
+++ src/markdeck/lib/render-asciiart-filter.lua
@@ -127,16 +127,16 @@ local renderer = {
         if attrs[1] then
             attrs = attrs[1][2]
         end
-        params = {"-c", "svgbob"}
-        for _, w in pairs(shlex(attrs)) do
-            table.insert(params, w)
-        end
-        return {"bash", params, text}, "svg"
---        params = {}
+--        params = {"-c", "svgbob"}
 --        for _, w in pairs(shlex(attrs)) do
 --            table.insert(params, w)
 --        end
---        return {"svgbob", params, text}, "svg"
+--        return {"bash", params, text}, "svg"
+        params = {}
+        for _, w in pairs(shlex(attrs)) do
+            table.insert(params, w)
+        end
+        return {"svgbob", params, text}, "svg"
     end,
 }
 
