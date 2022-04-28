c43c99c6acee7219d945a59d75b0d55549658356
Arne Hilmann <arne.hilmann@codecentric.de>
2022-04-28 19:55:53 +0200

    copy target-specific pandoc binary to docroot

--- build.rs
+++ build.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::env;
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let target = env::var("TARGET").unwrap();
+    print!("building target {target}\n");
+
+    let source = format!("src/docroot/pandoc-{target}");
+    let target = "src/docroot/main/.tools/pandoc";
+
+    print!("copying pandoc {source} to {target}\n");
+
+    fs::copy(source, target)?;
+    Ok(())
+}
